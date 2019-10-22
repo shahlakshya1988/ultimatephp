0033fcf2f9c8f5b10016a1a4210da43fa86d06a7
Lakshya Shah <shahlakshya1988@gmail.com>
2019-10-22 15:44:08 +0530

    16. Exercise 2: Print Your Name on Web Page using PHP, 17. Exercise 3: Use all PHP Tags Learned So Far

---
+++ 02basicsofphp/Exercise 2 - Print your Name with PHP/HelloWorldMe.php
@@ -0,0 +1,7 @@
+<?php 
+echo "Hello World<br>";
+print "Programming, Programming, Programming<br>";
+?><br>
+<?="Programming"?><br>
+<?="Eating"?><br>
+<?="Sleeping"?><br>
---
+++ 02basicsofphp/Exercise 3 - Use all PHP tags and Comments/HelloComments.php
@@ -0,0 +1,10 @@
+<?php 
+	/**
+	THIS PROGRAM DEMOSTRATED USE
+	OF VARIOUS PHP TAGS AS WELL AS COMMENTS
+	**/
+?>
+<?php echo "Hello World!"; // this echos hello world ?><br>
+<?php print "Hello World!"; // this prints hello world ?><br>
+<?="Lakshya Shah" # my name?><br>
+<?="Love To Do Programming" # my aspirations ?>
