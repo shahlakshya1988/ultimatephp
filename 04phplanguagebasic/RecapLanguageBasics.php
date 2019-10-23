c6212fb40ea50e44783e72193652fc5ba7463f6b
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-23 08:40:35 +0530

    37. Example: Recap Language Basics

---
+++ 04phplanguagebasic/RecapLanguageBasics.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?="Document Title Goes Here"?></title>
+</head>
+<body>
+    <?php echo "<h1>Heading 1</h1>"; ?>
+    <?php echo "\n this is for c\ronsole\n"; ?>
+    <?php echo "\r this is carriage return \r"; ?>
+    <?php echo "\t This is tab \t tab character";?>
+    <?php echo "This is HTML tab &nbsp; <br> is used for new line or carriage return"; ?>
+</body>
+</html>
