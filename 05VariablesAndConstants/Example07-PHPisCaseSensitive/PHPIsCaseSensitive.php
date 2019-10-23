8cde12edbdc256ce8162510046c7fa6dbd01cda9
Lakshya Shah <shahlakshya1988@gmail.com>
2019-10-23 15:27:39 +0530

    48. PHP Case Sensitive

---
+++ 05VariablesAndConstants/Example07-PHPisCaseSensitive/PHPIsCaseSensitive.php
@@ -0,0 +1,19 @@
+<?php echo "PHP Tags are in small"; ?>
+<br>
+<?PHP ECHO "PHP tags are in caps"; ?>
+<br>
+<?php 
+function ADD(){
+    ECHO "<br>Function name add <br>";
+}
+ADD();
+add();
+adD();
+?>
+<?php 
+$myname = "Lakshya";
+echo $myname;
+echo "<br>";
+echo $MYNAME;
+$MYNAME = "sHAH";
+?>
