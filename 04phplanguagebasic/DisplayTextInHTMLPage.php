e182188e35b7369a8fc9427daa1e46ee95487115
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-23 08:49:25 +0530

    Excercise for displying HTML and PHP

---
+++ 04phplanguagebasic/DisplayTextInHTMLPage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?="Generate HTML Tags With PHP" ?></title>
+</head>
+<body>
+    <?php echo "<h1>About Me</h1>"; ?>
+    <?php echo "<h2>I'a Lakshya, Love to learn and work In PHP</h2>"; ?>
+    <?="<br><hr><br>" ?>
+    <?="<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem corrupti laudantium id, obcaecati incidunt adipisci aut error est doloribus modi labore, aspernatur aliquid, vero animi. Excepturi laudantium molestias totam molestiae</p>"?>
+
+</body>
+</html>
