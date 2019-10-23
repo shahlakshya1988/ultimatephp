013d1e355b0e124781acd7188f21ff1c35d3e084
Lakshya Shah <shahlakshya1988@gmail.com>
2019-10-23 12:00:01 +0530

    45. Single Quotes vs Double Quotes

---
+++ 05VariablesAndConstants/Example04-SingleQuotesDoubleQuotes/SinglevsDoubleQuotes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?="When To Use Single Quotes / Double Quotes"?></title>
+</head>
+<body>
+    <?php 
+    echo PHP_EOL;
+    $message = "This message is in Double quotes";
+    echo $message.PHP_EOL;
+    $message = 'This message is in Single quotes';
+    echo $message.PHP_EOL;
+    $somenumber = 2019;
+    $message = "Year : {$somenumber}";
+    echo $message.PHP_EOL;
+    $message = "Year : $somenumber";
+    echo $message.PHP_EOL;
+    $message = "Year : $somenumber";
+    echo $message.PHP_EOL;
+    ECHO "<bR>";
+    echo "$message"." ".'$message'.PHP_EOL;
+    
+    ?>
+</body>
+</html>
