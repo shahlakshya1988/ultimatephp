07eb12932cf7a335983695116a30180b40b91c43
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-23 08:30:35 +0530

    35. PHP Escaping Characters

---
+++ 04phplanguagebasic/EscapeCharactersInConsole.php
@@ -0,0 +1,17 @@
+<?php  
+/***
+ * \ - This is a Escaping Char
+ * \n - New Line
+ * \r - Carriage Return
+ * \t - Tab
+ * \$ - Dollar
+ * \' - Singl Quotes
+ * \" - Double Quotes
+ * \\ - Backslash
+ */
+echo "\nThis isn't the way We Want To Print <br>";
+echo 'This isn\'t the way We Want To Print <br>';
+echo "This is \n new line char \n is new line";
+echo "\n We have tab \t\t here. \n New Line over here";
+echo '\n We have tab \t\t here. \n New Line over here';
+echo "This is a \"Special\" Very Special \' Char\'";
---
+++ 04phplanguagebasic/EscapeCharactersInHTML.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * \n new line <br>
+ * \r carriage return <br>
+ * \t tab &nbsp;
+ */
+
+ echo '<br>This is having new line <br>';
+ echo "&nbsp;Hello&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;World";
