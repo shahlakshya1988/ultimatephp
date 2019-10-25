432384b66457bdc82e1b9299e43c3d04dd4b5cea
Lakshya Shah <shahlakshya1988@gmail.com>
2019-10-25 16:41:10 +0530

    60. Magic Constants

---
+++ 05VariablesAndConstants/Example19-MagicConstant/MagicConstants.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "__LINE__".__LINE__;
+echo "<br>";
+echo "__FILE__".__FILE__;
+echo "<br>";
+echo "__DIR__".__DIR__;
+echo "<br>";
+echo "__LINE__".__LINE__;
+echo "<br>";
+echo "__LINE__".__LINE__;
+echo "<br>";
