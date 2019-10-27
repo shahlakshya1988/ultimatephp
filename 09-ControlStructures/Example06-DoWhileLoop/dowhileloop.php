793ab5a3676685baffa80ea2d3062402dca605fb
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-27 19:40:02 +0530

    105. Loops: do...while Statement

---
+++ 09-ControlStructures/Example06-DoWhileLoop/dowhileloop.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * do{
+ *  // code will be excecuted, then the condition is checked, depending on condition the loop will repeat
+ * }while(Condition); // note this will have semicolon
+ */
+$value = 11;
+do{
+    echo $value++.PHP_EOL;
+}while($value<=10);
+echo $value;
+ECHO PHP_EOL;
+ECHO PHP_EOL;
+$value = 1;
+do{
+    echo $value++.PHP_EOL;
+}while($value<=10);
+echo $value;
