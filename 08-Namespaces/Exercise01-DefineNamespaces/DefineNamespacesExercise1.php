b4e64b487e9f583f977247588c3b86ded4c25a5e
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-27 15:13:06 +0530

    96. Exercise 1 - Define Namespaces

---
+++ 08-Namespaces/Exercise01-DefineNamespaces/DefineNamespacesExercise1.php
@@ -0,0 +1,10 @@
+<?php
+include "NamespaceConstant1.php";
+echo "Number Of Subjects ".StudentConstants\TOTAL_SUBJECTS;
+echo PHP_EOL;
+echo "Total Marks ".StudentConstants\TOTAL_MARKS;
+echo PHP_EOL;
+echo "Passing Marks ".StudentConstants\PASSING_MARKS;
+echo PHP_EOL;
+echo "From Namespace ".StudentConstants\NAMESPACENAME;
+echo PHP_EOL;
