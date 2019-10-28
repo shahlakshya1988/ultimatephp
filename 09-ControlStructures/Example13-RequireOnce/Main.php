961f8a5cfced602faea894b8457845accba5f5fb
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-28 09:09:47 +0530

    112. require_once

---
+++ 09-ControlStructures/Example13-RequireOnce/Main.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * require_once will only include the file only once, where else require can include it again and again.
+ */
+
+require_once  "header.php";
+require_once  "menu.php";
+require_once  "menu.php";
+require_once  "menu.php";
+require_once  "menu.php";
+require "footer.php";
+require "footer.php";
+require "footer.php";
+
