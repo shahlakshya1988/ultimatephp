158b9db8585eb9dc4de7bcf4e4205c7bbdc0bd35
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-28 11:36:16 +0530

    122. String Functions: strlen() - Find Length of String

---
+++ 10-Strings/Example04-Strlen/StringLength.php
@@ -0,0 +1,16 @@
+<?php
+$string = "Lakshya";
+echo strlen($string);
+echo PHP_EOL;
+$string2= "";
+echo strlen($string2);
+echo PHP_EOL;
+echo @strlen($string3);
+echo PHP_EOL;
+$string4 = null;
+echo strlen($string4);
+echo PHP_EOL;
+$string5 =" ";
+echo strlen($string5);
+echo PHP_EOL;
+echo PHP_EOL;
