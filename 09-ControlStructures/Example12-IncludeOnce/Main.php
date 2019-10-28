4a449d9114835172f879fc7129514ead8058e196
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-28 09:07:53 +0530

    111. include_once

---
+++ 09-ControlStructures/Example12-IncludeOnce/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * include_once will only include the file only once, whereelse include can include it again and again.
+ */
+
+include_once  "header.php";
+include_once  "menu.php";
+include_once  "menu.php";
+include_once  "menu.php";
+include_once  "menu.php";
+include "footer.php";
+include "footer.php";
+include "footer.php";
+
