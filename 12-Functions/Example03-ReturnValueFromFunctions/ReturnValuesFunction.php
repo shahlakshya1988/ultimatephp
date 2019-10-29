7c83b73d39418b5d3fe4b48181311bdcf3d7d20f
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-29 08:24:38 +0530

    148. Return Value from Functions

---
+++ 12-Functions/Example03-ReturnValueFromFunctions/ReturnValuesFunction.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * function function_name(param1,param2,....,paramn) : return type{
+
+ * return something;
+ *
+ * }
+ */
+declare(strict_type=1);
+function add(int $a,int $b) : int{
+    return $a+$b;
+    //return "Hello World"; //$a+$b;
+}
+
+echo add(23,20);
+echo PHP_EOL;
+echo PHP_EOL;
+function getEvenNumbers(int $limit):array{
+    $array = [];
+    for($index = 0; $index<=$limit;$index++){
+        if($index % 2 ==0){
+            $array[]=$index;
+        }
+    }
+    return $array;
+}
+print_r(getEvenNumbers(20));
