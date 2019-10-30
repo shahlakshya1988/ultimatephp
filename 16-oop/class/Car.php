671f4095f0ac2bc660768e74c3f876ced946ab28
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-30 20:33:20 +0530

    189. How to Define a Class

---
+++ 16-oop/class/Car.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Class Name First Letter should sharts with capital Letters.
+ * File Name should match with Class Name. Easy to Follow.
+ * One File should have One Class. You can have multiple 'class' in the keyword used to define a class
+ * classis opened and closed with {} just like functions
+ */
+class Car{
+    function  helloclass(){
+        echo "Hello From Car Class".PHP_EOL;
+    }
+    // echo "Hello World"; // error, always write in function, class have only two things methods, properties
+} // class Car
---
+++ 16-oop/class/Database.php
@@ -0,0 +1,27 @@
+<?php
+class Database{
+    // Define Variables/Properties
+    public $dbConnection;
+    public $records;
+    // This function can be used to open the connection with the database
+    public function openConnection():bool{
+
+    }
+    // This function can be used to fetch records from database
+    function fetchRecord($id):array{
+
+    }
+
+    // Insert Records
+    function insertRecords($data):bool{
+
+    }
+    // update Records
+    function updateRecords($id,$newData):bool{
+
+    }
+    //Delete Records
+    function deleteRecords($id):bool{
+
+    }
+} // class Database
